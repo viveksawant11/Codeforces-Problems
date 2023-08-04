b1753383c41ec05c8bf44662828f0ff99cb155c0
viveksawant11 <118084499+viveksawant11@users.noreply.github.com>
2023-08-04 18:48:28 +0530

    Add files via upload

---
+++ Beautiful_Matrix.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a(0);
+    for (int i=1;i<=5;i++)
+    {
+        for (int j=1;j<=5;j++)
+        {
+            cin>>a;
+            if (a==1)
+                cout<<abs(i-3)+abs(j-3);
+        }
+    }
+    
+    return 0;
+}
+ 
