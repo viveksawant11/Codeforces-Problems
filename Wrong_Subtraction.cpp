7527391c3901f1f70b5449794660b908fe002a88
viveksawant11 <118084499+viveksawant11@users.noreply.github.com>
2023-08-08 18:29:00 +0530

    Add files via upload

---
+++ Wrong_Subtraction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    
+    while(k--)
+    {
+        if ( (n%10)!=0 )
+            n-=1;
+        else 
+            n/=10;
+    }
+    cout<<n<<endl;
+    return 0;
+}
